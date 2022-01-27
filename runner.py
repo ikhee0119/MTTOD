8fa342713022c95da196b37de582e81956fee077
ikhee <ikhee@0587291262.wireless.umich.net>
2022-01-27 04:59:47 -0500

    Role back

--- runner.py
+++ runner.py
@@ -454,7 +454,6 @@ class MultiWOZRunner(BaseRunner):
         reporter = Reporter(1000000, self.cfg.model_dir)
 
         torch.set_grad_enabled(False)
-        step_outputs = None
 
         for batch in tqdm(dev_iterator, total=num_steps, desc="Validaction"):
             start_time = time.time()
